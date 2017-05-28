5a9f18c63fe3a4299f31ab475f898c5087180328
Eugene Hauptmann <eugene.hp2012@gmail.com>
2017-05-27 22:11:43 -0700

    Update Buttons.js
    added accessibilityTraits

--- components/Buttons.js
+++ components/Buttons.js
@@ -3,7 +3,7 @@ import { View, TouchableOpacity, Text } from 'react-native';
 
 const SymbolButton = ({ isLight, size, onPress, style, textStyle, children }) => (
   <View style={{ height: size, width: size, justifyContent: 'center', ...style }}>
-    <TouchableOpacity style={{ flex: 0 }} onPress={onPress}>
+    <TouchableOpacity accessible={true} testID="SymbolButton" accessibilityTraits="button" style={{ flex: 0 }} onPress={onPress}>
       <Text style={{ textAlign: 'center', fontSize: size / 1.7, ...textStyle }}>{children}</Text>
     </TouchableOpacity>
   </View>
@@ -11,7 +11,7 @@ const SymbolButton = ({ isLight, size, onPress, style, textStyle, children }) =>
 
 const TextButton = ({ isLight, size, onPress, textStyle, children }) => (
   <View style={{ flex: 0 }}>
-    <TouchableOpacity style={{ flex: 0 }} onPress={onPress}>
+    <TouchableOpacity accessible={true} testID="TextButton" accessibilityTraits="button" style={{ flex: 0 }} onPress={onPress}>
       <Text style={{ fontSize: size / 2.5, ...textStyle }}>{children}</Text>
     </TouchableOpacity>
   </View>
